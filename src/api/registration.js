3232092a5603d0a06fc713f0b859f57ab8b80c5d
vladb95_ <vladb95z-soft@yandex.ru>
2017-03-28 23:07:09 +0300

    added some changes

--- src/api/registration.js
+++ src/api/registration.js
@@ -1,8 +1,10 @@
 import resource from 'resource-router-middleware';
+import { Router } from 'express';
 import models from '../models';
 
 export default ({ config }) => resource({
     id: 'registaration',
+
     create({ body }, res) {
         models.Users.create(body)
             .then(data => {
